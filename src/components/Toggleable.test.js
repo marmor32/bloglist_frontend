5e7eeb242fb500cc657d0ecff874f24a82bd7aec
marmor32 <bhmi1994@gmail.com>
2021-08-30 12:43:00 +0300

    add toggleabe test file

---
+++ src/components/Toggleable.test.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, fireEvent } from '@testing-library/react'
+import Toggleable from ./Toggleable
+
+test()

552149efad25878d4ea031432f788ff09b611331
marmor32 <bhmi1994@gmail.com>
2021-08-30 13:58:42 +0300

    toggleable to togglable

--- src/components/Togglable.test.js
+++ src/components/Togglable.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent } from '@testing-library/react'
-import Toggleable from './Toggleable'
+import Togglable from './Togglable'
 
 describe('<Togglable />', () => {
   let component

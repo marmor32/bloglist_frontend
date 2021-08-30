64e1ba9d280f19888eac4c049ca74c9db71dcad6
marmor32 <bhmi1994@gmail.com>
2021-08-30 13:55:03 +0300

    add toggleable tests

--- src/components/Toggleable.test.js
+++ src/components/Toggleable.test.js
@@ -3,4 +3,35 @@ import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent } from '@testing-library/react'
 import Toggleable from './Toggleable'
 
-test()
+describe('<Togglable />', () => {
+  let component
+
+  beforeEach(() => {
+    component = render(
+      <Togglable buttonLabel="show...">
+        <div className="testDiv" />
+      </Togglable>
+    )
+  })
+
+  test('renders its children', () => {
+    expect(
+      component.container.querySelector('.testDiv')
+    ).toBeDefined()
+  })
+
+  test('at start the children are not displayed', () => {
+    const div = component.container.querySelector('.togglableContent')
+
+    expect(div).toHaveStyle('display: none')
+  })
+
+  test('after clicking the button, children are displayed', () => {
+    const button = component.getByText('show...')
+    fireEvent.click(button)
+
+    const div = component.container.querySelector('.togglableContent')
+    expect(div).not.toHaveStyle('display: none')
+  })
+
+})
